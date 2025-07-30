fad8a4a90ee85f96139a6229db3eebea1bc66102
Santailha <vendas@santailha.com.br>
2025-07-30 15:51:26 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLGj0JQiNUGXMXsnKwgRhrMs9S_--QIkU",
+  authDomain: "matriz-9box-rh.firebaseapp.com",
+  projectId: "matriz-9box-rh",
+  storageBucket: "matriz-9box-rh.firebasestorage.app",
+  messagingSenderId: "89333414990",
+  appId: "1:89333414990:web:22d24f9dc6dd9d698b171f"
+};
+// Exportamos a configuração para que outros arquivos possam usá-la.
+export { firebaseConfig };
